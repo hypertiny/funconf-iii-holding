6e4ee4f0d905fdc3d8a2c2317adda547a8ff4f90
Paul Campbell <paul@rslw.com>
2012-03-31 19:37:42 +0100

    js validation

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
 jQuery(function(){
   $('#interest-form').submit(function(){
-    $.post($(this).attr('action') + '.js', $(this).serialize())
-    $(this).slideUp()
+    email = $('#email').val()
+    email_regex = /^[a-zA-Z0-9.!#$%&'*+/=?\^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+    if($.trim(email) != '' && email_regex.test(email))
+    {
+      $('#message').text(email + ' has been added to the list.')
+      $.post($(this).attr('action') + '.js', $(this).serialize())
+      $('#form-fields').slideUp()
+    }
+    else
+    {
+      $('#message').text('Please enter a valid email address:')
+    }
+      
     return false
   })
 })
