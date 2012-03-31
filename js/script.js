7ac61790872b10806c4b7839159586552724bd79
Paul Campbell <paul@rslw.com>
2012-03-31 19:11:26 +0100

    fold up

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
-
-
-
-
-
+jQuery(function(){
+  $('#interest-form').submit(function(){
+    $.post($(this).attr('action') + '.js', $(this).serialize())
+    $(this).slideUp()
+    return false
+  })
+})
