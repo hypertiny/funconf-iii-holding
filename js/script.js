3bd1b0ac00825aad4d0c3e4beedd1f01a792e081
Paul Campbell <paul@rslw.com>
2012-05-22 21:16:08 +0100

    funconf iii site with holding in a subdir

--- js/script.js
+++ js/script.js
@@ -1,18 +1,42 @@
-jQuery(function(){
-  $('#interest-form').submit(function(){
-    email = $('#email').val()
-    email_regex = /^[a-zA-Z0-9.!#$%&'*+/=?\^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-    if($.trim(email) != '' && email_regex.test(email))
-    {
-      $('#message').text(email + ' has been added to the list.')
-      $.post($(this).attr('action') + '.js', $(this).serialize())
-      $('#form-fields').slideUp()
-    }
-    else
-    {
-      $('#message').text('Please enter a valid email address:')
-    }
-      
-    return false
-  })
-})
+
+$('#yes').click(function() {
+  
+  $('#yes-no').addClass('hidden');
+  $('#the-island-wash').addClass('hidden');
+  $('#the-island').addClass('anim-shake');
+  $('#delorean').addClass('anim-shake2');
+  $('#tickets').addClass('anim-ticket-rise');
+  $('#ticket-waves').addClass('anim-ticket-waves');
+  
+  return false;
+  
+});
+
+$('#no').click(function() {
+  
+  $('#yes-no').addClass('hidden');
+  $('#the-island-wash').addClass('hidden');
+  $('#the-island').addClass('anim-shake-long');
+  $('#compass').addClass('anim-fadeout');
+  $('#sky p').addClass('anim-fadeout');
+  $('#burst').addClass('anim-fadeout');
+  $('#helicopter').addClass('anim-fadeout');
+  $('h1').addClass('anim-fadeout');
+  $('#ship').addClass('anim-fadeout');
+  $('#tentacle').addClass('anim-fadeout');
+  $('#tentacle-sea').addClass('anim-fadeout');
+  $('#llama').addClass('anim-fadeout');
+  $('#llama-bubbles').addClass('hidden');
+  $('#fishies').addClass('hidden');
+  $('#waves').addClass('anim-fadeout');
+  $('#smoke').addClass('hidden');
+  $('#delorean').addClass('anim-delorean');
+  $('#delorean-shadow').addClass('anim-delorean-shadow');
+  $('#delorean-container').addClass('anim-delorean-container');
+  $('#delorean2').addClass('anim-delorean2');
+  $('#whiteout').addClass('anim-whiteout');
+  $('#whiteout-full').addClass('anim-whiteout-full');
+
+  return false;
+
+});
